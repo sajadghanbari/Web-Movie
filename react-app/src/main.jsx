8ce0c79615baf7e24079d1ce2d94035fe41b76cf
Sajjad Ghanbari <a.s.ghanbari.j@gmail.com>
2025-07-17 14:56:59 +0330

    route created for home and movies page

--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -1,9 +1,26 @@
 import { StrictMode } from 'react'
+import {createBrowserRouter , RouterProvider} from 'react-router'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import Home from './components/home.jsx';
+import Movies from './components/Movies.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {index: true, element: <Home />},
+      {
+        path: '/movies',
+        element: <Movies />
+      }
+    ]
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
