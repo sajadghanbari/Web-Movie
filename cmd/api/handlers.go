ef0e64944c1ec0fc3c788a306b09ee7d9a5ed090
Sajjad Ghanbari <a.s.ghanbari.j@gmail.com>
2025-07-22 18:23:16 +0330

    password confirmation

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"errors"
-	"log"
 	"net/http"
 	// "github.com/go-chi/chi/middleware"
 	// "github.com/go-chi/chi/v5"
@@ -52,10 +51,16 @@ func (app *application) authenticate(w http.ResponseWriter, r *http.Request){
 		return
 	}
 
+	valid, err := user.PasswordMatches(requestPayload.Password)
+	if err != nil || !valid {
+		app.errorJSON(w, errors.New("invalid credentials"),http.StatusBadRequest)
+		return
+	}
+
 	u := jwtUser{
-		ID: 1,
-		FirstName: "Admin",
-		LastName: "User",
+		ID: user.ID,
+		FirstName: user.FirstName,
+		LastName: user.LastName,
 	}
 
 	tokens, err := app.auth.GenerateTokenPair(&u)
@@ -63,9 +68,10 @@ func (app *application) authenticate(w http.ResponseWriter, r *http.Request){
 		app.errorJSON(w, err)
 		return
 	}
-	log.Println(tokens.Token)
+	
 	refreshCookie := app.auth.GetRefreshCookie(tokens.RefreshToken)
 	http.SetCookie(w , refreshCookie)
-	w.Write([]byte(tokens.Token))
+	
+	app.writeJSON(w,http.StatusAccepted,tokens)
 
 }
