959bdcef040f7ff36dcbaa2e5ab419bac0ed662d
Sajjad Ghanbari <a.s.ghanbari.j@gmail.com>
2025-07-22 16:32:14 +0330

    generating jwt token

--- cmd/api/auth.go
+++ cmd/api/auth.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/golang-jwt/jwt/v4"
@@ -30,4 +31,43 @@ type TokenPairs struct {
 
 type Claims struct{
 	jwt.RegisteredClaims
+}
+
+func (j *Auth) GenerateTokenPair(user *jwtUser) (TokenPairs , error){
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	claims := token.Claims.(jwt.MapClaims)
+	claims["name"] = fmt.Sprintf("%s %s",user.FirstName,user.LastName)
+	claims["sub"] = fmt.Sprint(user.ID)
+	claims["aud"] = j.Audience
+	claims["iss"] = j.Issuer
+	claims["iat"] = time.Now().UTC().Unix()
+	claims["typ"] = "JWT"
+
+	claims["exp"] = time.Now().UTC().Add(j.TokenExpiry).Unix()
+
+	signedAccessToken , err := token.SignedString([]byte(j.Secret))
+	if err != nil {
+		return TokenPairs{} , err
+	}
+
+	refreshToken := jwt.New(jwt.SigningMethodHS256)
+	refreshTokenClaims := refreshToken.Claims.(jwt.MapClaims)
+	refreshTokenClaims["sub"] = fmt.Sprint(user.ID)
+	refreshTokenClaims["iat"] = time.Now().UTC().Unix()
+
+	refreshTokenClaims["exp"] = time.Now().UTC().Add(j.RefreshExpiry).Unix()
+
+	signedRefreshToken, err := refreshToken.SignedString([]byte(j.Secret))
+	if err != nil{
+		return TokenPairs{},err
+	}
+	var tokenPairs = TokenPairs {
+		Token: signedAccessToken,
+		RefreshToken: signedRefreshToken,
+	}
+
+	return tokenPairs , nil
+
+
 }
